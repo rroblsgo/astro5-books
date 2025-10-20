f2feb09f4529032517bd8c31dbfef4a3e69374d5
Ricardo Robles <rroblesgo@gmail.com>
2025-10-20 12:46:02 +0200

    Final Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,32 @@
 // @ts-check
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 import { dirname, resolve } from "path";
 import { fileURLToPath } from "url";
+
+import node from "@astrojs/node";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+
+  env: {
+    schema: {
+      SHOW_BUY_BUTTON: envField.boolean({
+        default: true,
+        context: "server",
+        access: "public",
+      }),
+      SCORE_API_ENDPOINT: envField.string({
+        context: "server",
+        access: "public",
+      }),
+    },
+  },
+
   vite: {
     plugins: [tailwindcss()],
     resolve: {
@@ -17,4 +35,8 @@ export default defineConfig({
       },
     },
   },
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
